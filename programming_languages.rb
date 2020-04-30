26cee0e76617ac74779da22c9f37ddbaee9807f4
Pat McClernan <patmccler@gmail.com>
2020-04-30 16:01:07 -0400

    build out reformat_languages method

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  langs_by_name = {}
+
+  languages.each do |style, langs|
+    langs.each do |lang, type|
+
+      curr_styles = langs_by_name[lang] ? langs_by_name[lang][:style] : []
+      curr_styles << style
+
+      langs_by_name[lang] =
+      {
+        type: type[:type],
+        style: curr_styles
+      }
+    end
+  end
+  langs_by_name
 end
