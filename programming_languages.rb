6212cdb00f9cc9021613e7b84e109d87581ea797
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-04 09:32:00 -0400

    lesson complete

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
-  # your code here
+  hash = {}
+  languages.each do |k0,v0|
+    v0.each do |k1,v1|
+      if hash.key?(k1)
+        hash[k1][:style].push(k0)
+      else
+        hash[k1] = { type: v1[:type], style: [k0]}
+      end
+    end
+  end
+  hash
 end
