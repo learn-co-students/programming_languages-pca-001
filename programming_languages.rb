d8d74a8f80553583aebdd876f812e8c7575ad858
Jo Fischer <josephinefischer9@gmail.com>
2020-05-11 12:02:36 -0400

    very hard

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |key, value|
+    value.each do |key1, value1|
+      if new_hash.include?(key1)
+        new_hash[key1][:style] << key
+      else value1.each do |type, value|
+        new_hash[key1] = {:type => value, :style => [key]}
+      end
+      end
+    end
+  end
+new_hash
 end
